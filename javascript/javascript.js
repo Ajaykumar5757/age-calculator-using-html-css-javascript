a9ea319136e63d01bf48c78610b2d3a52bc14152
Ajaykumar A <ajaykumarak2703@gmail.com>
2024-08-04 18:45:37 +0530

    Add files via upload

---
+++ javascript/javascript.js
@@ -0,0 +1,29 @@
+function calculateage(){
+    var dob=document.getElementById("dob").value;
+
+    var dateofbirth=new Date(dob);
+    var today=new Date();
+    
+    var year=today.getFullYear()-dateofbirth.getFullYear();
+    var month=today.getMonth()-dateofbirth.getMonth();
+    var date=today.getDate()-dateofbirth.getDate();
+    
+    if(dob==""||dob==null){
+        alert("please input field should not be empty");
+    }
+    else if(dateofbirth>today){
+        alert("Please enter the date before "+today.getDate()+"/"+today.getMonth()+"/"+today.getFullYear());
+    }
+    else{
+        if(month<0){
+            year--;
+            month=month+12;
+        }
+        
+        if(date<0){
+            month--;
+            date=date+new Date(today.getFullYear(),today.getMonth(),0).getDate();
+        }
+        document.getElementById("age").innerHTML=`Your age is ${year} years, ${month} months and ${date} days`;
+    }
+}
